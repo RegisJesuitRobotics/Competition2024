43516ed44875cd0db60cd155519acbbf0a5b7f66
nickturner <nins97@outlook.com>
2024-01-15 14:07:59 -0700

    Oliver changed something on my computer and I don't know what he did.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Competition2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "461295039e3a3fd9da8966a0939d8c46154b1433";
-  public static final String GIT_DATE = "2024-01-15 12:49:52 MST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "a98c4827d0b68d4318ecc27a42ce002eba7a2fe0";
+  public static final String GIT_DATE = "2024-01-15 13:35:06 MST";
   public static final String GIT_BRANCH = "wrist";
-  public static final String BUILD_DATE = "2024-01-15 13:34:06 MST";
-  public static final long BUILD_UNIX_TIME = 1705350846944L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-01-15 13:42:27 MST";
+  public static final long BUILD_UNIX_TIME = 1705351347460L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
--- src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
@@ -50,6 +50,6 @@ public class WristSubsystem extends SubsystemBase {
 
         setVoltage(combinedOutput);
 
-        
+
     }
 }
