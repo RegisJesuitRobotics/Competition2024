ba4712fbc383d5a98db7d19a7c257e9390a9958b
nickturner <nins97@outlook.com>
2024-01-20 14:20:37 -0700

    clean up

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Competition2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 33;
-  public static final String GIT_SHA = "c309aa404556a150f662d9478c11bccb7e927b13";
-  public static final String GIT_DATE = "2024-01-20 14:01:22 MST";
+  public static final int GIT_REVISION = 34;
+  public static final String GIT_SHA = "819191306253c712f35343c71890f81e7056802d";
+  public static final String GIT_DATE = "2024-01-20 14:05:38 MST";
   public static final String GIT_BRANCH = "wrist";
-  public static final String BUILD_DATE = "2024-01-20 14:04:57 MST";
-  public static final long BUILD_UNIX_TIME = 1705784697343L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-01-20 14:19:46 MST";
+  public static final long BUILD_UNIX_TIME = 1705785586691L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
