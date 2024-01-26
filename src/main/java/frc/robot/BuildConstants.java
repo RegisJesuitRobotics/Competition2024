363e41790c5e1ef6f234de11da9a17bb0b6201e9
nickturner <nins97@outlook.com>
2024-01-25 17:08:52 -0700

    gradlefix maybe

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Competition2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 20;
-  public static final String GIT_SHA = "e09c85412f30e6021b349a844a72376fc1b6071d";
-  public static final String GIT_DATE = "2024-01-15 14:35:46 MST";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "44467cc3c16af067f4d1db27f78ef1775da331d4";
+  public static final String GIT_DATE = "2024-01-25 17:03:49 MST";
   public static final String GIT_BRANCH = "photonvision";
-  public static final String BUILD_DATE = "2024-01-25 16:05:36 MST";
-  public static final long BUILD_UNIX_TIME = 1706223936923L;
+  public static final String BUILD_DATE = "2024-01-25 17:08:37 MST";
+  public static final long BUILD_UNIX_TIME = 1706227717883L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
